aa73a01dcb8250752314b625f91217f2af733df6
KHEDIR SEID MUHAMMED <119576429+KEDIRS@users.noreply.github.com>
2025-01-25 10:54:51 +0300

    Update functions.js

--- js-basics/functions.js
+++ js-basics/functions.js
@@ -1,6 +1,7 @@
-function greetUser(name) {
-    return `Hello, ${name}! Welcome to JavaScript!`;
+function greetUser(name)// Define a function named 'greetUser' that takes a parameter, 'name'
+{
+    return `Hello, ${name}! Welcome to JavaScript!`;// i have Used template literals to return a greeting message with the given name.
 }
 
-const greetingMessage = greetUser("Kedir");
-console.log(greetingMessage);
+const greetingMessage = greetUser("Kedir");// Call the 'greetUser' function with the argument "Kedir" and store the returned message in the variable 'greetingMessage'.
+console.log(greetingMessage);// The greetingMessage will be diplayed to the console.
