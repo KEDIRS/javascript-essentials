09f1b06d50da131a08cbc803df3e81e8a5017c26
KHEDIR SEID MUHAMMED <119576429+KEDIRS@users.noreply.github.com>
2025-01-25 11:04:44 +0300

    Update variables.js

--- js-basics/variables.js
+++ js-basics/variables.js
@@ -2,12 +2,16 @@ let name = "kedir seid"; //since it will change after
 let age = 23;//since it will change after
 const country = "Ethiopia";//since it remain unchanged over the script
 
+
+// Log the initial values of 'name', 'age', and 'country' to the console.
 console.log("Name:", name);
 console.log("Age:", age);
 console.log("Country:", country);
 
+// Log the initial values of 'name', 'age', and 'country' to the console.
 name = "Kedir seid";
 age = 24;
 
+// Log the updated values of 'name' and 'age' to the console.
 console.log("Updated Name:", name);
 console.log("Updated Age:", age);
