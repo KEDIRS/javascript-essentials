82c5d1be72925f2941e7a33594ee25e9b103e3ca
KHEDIR SEID MUHAMMED <119576429+KEDIRS@users.noreply.github.com>
2025-01-25 11:13:53 +0300

    Update arrays.js

--- js-basics/arrays.js
+++ js-basics/arrays.js
@@ -1,5 +1,6 @@
-const fruits = ["Apple", "Banana", "Cherry"];
+const fruits = ["Apple", "Banana", "Mango","Papaya"];// Declaring an array 'fruits' containing four fruit names.
 
-for (const fruit of fruits) {
+for (const fruit of fruits)// I have Used a 'for...of' loop to iterate through each element in the 'fruits' array. 
+{
     console.log(fruit);
-}
+}// Log each fruit to the console during each iteration.
