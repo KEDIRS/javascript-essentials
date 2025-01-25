a36d4f2805ec10894adc9b0f828f6e1e68661c4f
KHEDIR SEID MUHAMMED <119576429+KEDIRS@users.noreply.github.com>
2025-01-25 11:01:11 +0300

    Update objects.js

--- js-basics/objects.js
+++ js-basics/objects.js
@@ -2,8 +2,9 @@ const car = {
     brand: "Toyota",
     model: "Corolla",
     year: 2020
-};
+};// Define an object 'car' with properties: brand, model, and year of manufacture respectively.
 
 console.log("Car Brand:", car.brand);
 console.log("Car Model:", car.model);
 console.log("Car Year:", car.year);
+// Log the car's brand,model and year of manufacture respectively to the console.
