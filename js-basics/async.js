145ad84d392f64c6949309c49115279a639b2f1f
KHEDIR SEID MUHAMMED <119576429+KEDIRS@users.noreply.github.com>
2025-01-25 10:57:59 +0300

    Update async.js

--- js-basics/async.js
+++ js-basics/async.js
@@ -1,7 +1,9 @@
-console.log("Start");
+console.log("Start");// Log "Start" immediately to the console.
 
+// Seting a timeout to log "This message is delayed!" after 2 seconds (2000 milliseconds).
 setTimeout(() => {
-    console.log("This message is delayed!");
+    console.log("This message is delayed!"); // This will execute after the 2-second delay.
 }, 2000);
 
-console.log("End");
+
+console.log("End");// Log "End" immediately to the console.
